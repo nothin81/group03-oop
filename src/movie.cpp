c9ecd7c40989b17c5e81e419816c5c406a243268
thinh08 <thinhdao135@gmail.com>
2025-10-20 21:10:50 +0700

    stage bi thieu

--- src/movie.cpp
+++ src/movie.cpp
@@ -3,23 +3,71 @@
 using namespace std;
 
 class Movie {
-public:
+private:
     int id;
-    string name;
+
+    string title;
+    int release_year;
     string genre;
-    int duration;
-    // 01; John Wick 4; Action; 169
-    Movie setMovie(int id, string name, string genre, int duration);
+    int runtime;
+
+public:
+    Movie(); //constructor 
+    Movie(int id, string& title, const int release_year, const string& genre, int runtime);
+    ~Movie();
     void display();
+
+    void setTitle(string title);
+    void setGenre(string genre);
+    void setRuntime(int runtime);
+
+    int getId();
+    string getTitle();
+    string getGenre();
+    int getRuntime();
+
+    void addMovie();
 };
 
-Movie Movie::setMovie(int id, string name, string genre, int duration)
+Movie::Movie(int id, string& title, const int release_year, const string& genre, int runtime)
+    : id(id), title(title), release_year(release_year), genre(genre), runtime(runtime){}
+
+void Movie::setTitle(string title)
 {
-    this->id = id;
-    this->name = name;
-    this->genre = genre;
-    this->duration = duration;
+    this->title = title;
 }
+
+string Movie::getTitle() 
+{
+    return title;
+}
+
+void Movie::setGenre(string genre)
+{
+    this->genre = genre;
+}
+
+string Movie::getGenre() 
+{
+    return genre;
+}
+
+void Movie::setRuntime(int runtime)
+{
+    this->runtime = runtime;
+}
+
+int Movie::getRuntime() 
+{
+    return runtime;
+}
+
+void Movie::addMovie()
+{
+    cout << "Nhap vao ten phim: ";
+    std::getline 
+}
+
 void Movie::display()
 {
     cout << "[" << id << "] " << name << " - " << genre
