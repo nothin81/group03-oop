047cbc391bbf118aa574c43629b1126956d89e6b
thinh08 <thinhdao135@gmail.com>
2025-10-23 22:27:09 +0700

    them lai cinema class

--- src/cinema.h
+++ src/cinema.h
@@ -5,11 +5,31 @@
 #include <vector>
 #include <string>
 
-class Cinema
-{
+class Cinema {
 private:
+    int id;
+    std::string name;
+    std::string address;
+    int screeningRoom;
 
-
+public:
+    Cinema();
+    Cinema(int id, std::string name, std::string address, int screeningRoom);
+    
+    int getId() const;
+    std::string getName() const;
+    std::string getAddress() const;
+    int getScreeningRoom() const;
+    
+    void setId(int id);
+    void setName(std::string name);
+    void setAddress(std::string address);
+    void setScreeningRoom(int screeningRoom);
+    
+    void addCinema();
+    void display() const;
+    static void readFileEntry();
+    void writeFileEntry() const;
 };
 
-#endif // CINEMA_H
+#endif
