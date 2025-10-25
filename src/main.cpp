c43f0e1d466389e8c497ed5c7b3e7a94870e805f
thinh08 <thinhdao135@gmail.com>
2025-10-25 18:22:55 +0700

    added main interaction within menus

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,29 @@ void displayMovieMenu()
          << "Lua chon cua ban: ";
 }
 
+int movieMenu()
+{
+    int choice2;
+    Movie mov;
+
+    do {
+        displayMovieMenu();
+        cin >> choice2;
+        cin.ignore();
+        switch (choice2)
+        {
+            case 1:
+                mov.addMovie();
+                mov.writeFileEntry();
+                break;
+            case 2:
+                Movie::readFileEntry();
+                break;
+        }
+    } while (choice2 != 0);  //neu sai thi ve 0
+    return 0;
+}
+
 void displayCinemaMenu()
 {
     cout << "\nQUAN LY RAP CHIEU:\n"
@@ -36,6 +59,27 @@ void displayCinemaMenu()
          << "\t2. Xem danh sach rap\n"
          << "\t0. Quay lai\n"
          << "Lua chon cua ban: ";
+}
+
+int cinemaMenu()
+{
+    Cinema cine;
+    int choice2;
+    do {
+        displayCinemaMenu();
+        cin >> choice2;
+        cin.ignore();
+        switch (choice2) {
+            case 1:
+                cine.addCinema();
+                cine.writeFileEntry(); // bi trung lap ham nay
+                break;
+            case 2:
+                Movie::readFileEntry();
+                break;
+        }
+    } while (choice2 != 0);
+    return 0;
 }
 
 void displayBookingMenu()
@@ -47,16 +91,62 @@ void displayBookingMenu()
          << "Lua chon cua ban: ";
 }
 
+int bookingMenu()
+{
+    Booking bookings;
+    int choice2;
+    do {
+        displayBookingMenu();
+        cin >> choice2;
+        cin.ignore();
+        switch (choice2) {
+            case 1:
+                bookings.createBooking();
+                break;
+            case 2:
+                bookings.viewAllBookings();
+                break;
+        
+        }
+    }while (choice2 != 0);
+    return 0;
+}
+
 int main()
 {   
     Movie mov;
     Cinema cine;
-    Booking booking;
+    Booking bookings;
     int choice;
    
     while(true)
     {
-        
+        displayHeader();
+        displayMainMenu();
+        if ( !(cin >> choice) )
+        {
+            cout << "# Hay nhap vao ki tu";
+            cin.ignore();
+        }
+
+        switch (choice)
+        {
+            case 1:
+                if (movieMenu() == 0)
+                    break;
+                case 2:
+                if (cinemaMenu() == 0)
+                    break;
+            case 3:
+                if (bookingMenu() == 0)
+                    break;
+            case 0:
+                cout << "THOAT CHUONG TRINH!" << endl;
+                return 0;
+            default:
+                cout << "# Lua chon khong hop le!" << endl;
+        }
+
     }
     return 0;
 }
